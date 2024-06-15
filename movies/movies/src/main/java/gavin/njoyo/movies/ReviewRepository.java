e4480bfeb3584eff0bfcd07bb9e22fb4c0e39f17
GavinNjoyo <147401023+GavinNjoyo@users.noreply.github.com>
2024-06-15 19:01:35 +0100

    Add files via upload

---
+++ movies/movies/src/main/java/gavin/njoyo/movies/ReviewRepository.java
@@ -0,0 +1,7 @@
+package gavin.njoyo.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReviewRepository extends MongoRepository<Review, ObjectId>{
+}
